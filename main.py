b6fdc9f3309acb020bc351436948c163dad6607d
Hnatekmar <m.hnatma@gmail.com>
2017-04-04 09:57:57 +0200

    Další úprava pořád se bohužel neučí, tak jak má

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 from operator import xor
-import random
-import matplotlib.pyplot as plt
-from scipy.optimize import fmin_cg
+from scipy.optimize import *
 
 
 def sigmoid(x):
@@ -10,7 +8,7 @@ def sigmoid(x):
 
 
 def sigmoid_derivation(x):
-    return x * (1 + np.exp(-x))
+    return x * (1.0 + np.exp(-x))
 
 
 class NeuralNetwork:
@@ -18,10 +16,9 @@ class NeuralNetwork:
     Třívrstvý vícevrsvý perceptron
     """
     def __init__(self, layer1_size, layer2_size, layer3_size):
-        # Vygeneruji pole náhodně inicializovaných skrytých vrstev + bias
-        self.l1 = np.random.randn(layer1_size, layer2_size)
-        self.l2 = np.random.randn(layer2_size, layer1_size)
-        self.l3 = np.random.randn(layer3_size, layer2_size)
+        self.l1 = np.random.randn(layer1_size, layer2_size) * (0.12 * 2) - 0.12
+        self.l2 = np.random.randn(layer2_size, layer1_size) * (0.12 * 2) - 0.12
+        self.l3 = np.random.randn(layer3_size, layer2_size) * (0.12 * 2) - 0.12
 
     def reshape(self, x):
         l1_size = self.l1.shape[0] * self.l1.shape[1]
@@ -48,7 +45,7 @@ class NeuralNetwork:
         a3 = sigmoid(z2)
         z4 = self.l3.dot(a3)
         a4 = sigmoid(z4)
-        return a4
+        return np.argmax(a4) + 1
 
     def cost_fn_for_minimalization(self, x, *args):
         l1, l2, l3 = self.reshape(x)
@@ -79,16 +76,18 @@ class NeuralNetwork:
                 a3 = sigmoid(z2)
                 z3 = l3.dot(a3)
                 a4 = sigmoid(z3)
-                prediction = a4 # h(x_i)
+                # Predikce
+                prediction = np.matrix([a4]).transpose() # h(x_i)
                 y = input_data[i]["output"]
-                result += y * np.log(prediction) - np.dot((1 - y), np.log(1 - prediction))
+                result += (-y * np.log(prediction) - np.dot((1 - y), np.log(1 - prediction)))[0, 0]
+                prediction = a4
                 # chyba výstupu vrstvy
                 delta4 = prediction - y
                 delta3 = np.dot(l3.transpose(), delta4) * sigmoid_derivation(z3)
                 delta2 = np.dot(l2.transpose(), delta3) * sigmoid_derivation(z2)
-                l3_delta += learning_rate * np.dot(a4, delta4)
-                l2_delta += learning_rate * np.dot(a3, delta3)
-                l1_delta += learning_rate * np.dot(a2, delta2)
+                l3_delta += np.dot(delta4, a3.transpose())
+                l2_delta += np.dot(delta3, a2.transpose())
+                l1_delta += np.dot(delta2, a1.transpose())
         # Cost funkce bez regularizace
         result *= -(1/m)
 
@@ -100,29 +99,24 @@ class NeuralNetwork:
         l2_delta += (learning_rate / m) * l2
         l3_delta += (learning_rate / m) * l3
         return {
-            "cost": result[0],
+            "cost": result,
             "gradients": np.append(np.append(l1_delta.flatten(),
-                                             l2_delta.flatten()),
-                                             l3_delta.flatten())
-
+                                             l2_delta.flatten()), l3_delta.flatten())
         }
 
     def train(self, input_data, learning_rate, epochs):
         theta = np.append(np.append(self.l1.flatten(), self.l2.flatten()), self.l3.flatten())
-        random.shuffle(input_data)
-        result = fmin_cg(f=self.cost_fn_for_minimalization, fprime=self.gradients_for_minimalization, x0=theta,
-                         args=(learning_rate, input_data), maxiter=epochs)
-        self.l1, self.l2, self.l3 = self.reshape(result)
-
+        result = fmin_ncg(f=self.cost_fn_for_minimalization, fprime=self.gradients_for_minimalization, x0=theta,
+                         args=(learning_rate, input_data), maxiter=epochs, full_output=True)
+        self.l1, self.l2, self.l3 = self.reshape(result[0])
 
 if __name__ == '__main__':
-    neural_net = NeuralNetwork(2, 2, 1)
+    neural_net = NeuralNetwork(2, 2, 2)
     data = [np.array([x, y]).transpose() for x in range(0, 2) for y in range(0, 2)]
     input_data = [{
         "input": x,
-        "output": int(xor(x[0], x[1]))
+        "output": np.array([0, 1]) if xor(x[0], x[1]) else np.array([1, 0])
     } for x in data]
-    print(input_data)
-    neural_net.train(input_data, 0.00001, 90000)
+    neural_net.train(input_data, 1, 400)
     for data_point in data:
         print("nn(%s) = %s" % (data_point, neural_net.feed_forward(data_point)))
